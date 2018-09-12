d754c666b768fde684fc5bdcd2b6c60e362b16dc
Alex Crichton <alex@alexcrichton.com>
2018-09-12 11:54:49 -0700

    lol

---
+++ src/installer.rs
@@ -0,0 +1,38 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+use failure::{Error, ResultExt};
+
+pub fn install() -> Result<(), Error> {
+    let rustup = env::split_paths(&env::var_os("PATH").unwrap_or_default())
+        .map(|p| p.join("rustup").with_extension(env::consts::EXE_EXTENSION))
+        .find(|p| p.exists());
+    let rustup = match rustup {
+        Some(path) => path,
+        None => {
+            bail!("failed to find an installation of `rustup` in `PATH`, \
+                   is rustup already installed?");
+        }
+    };
+    let installation_dir = match rustup.parent() {
+        Some(parent) => parent,
+        None => bail!("can't install when `rustup` is at the root of the filesystem"),
+    };
+    let destination = installation_dir
+        .join("wasm-pack")
+        .with_extension(env::consts::EXE_EXTENSION);
+
+    if destination.exists() {
+        if !env::args().any(|arg| arg == "-f") {
+            bail!("existing wasm-pack installation found at `{}`, pass `-f` to \
+                   force installation over this file, otherwise aborting \
+                   installation now", destination.display());
+        }
+    }
+
+    let me = env::current_exe()?;
+    fs::copy(&me, &destination)
+        .with_context(|_| format!("failed to copy executable to `{}`", destination.display()))?;
+    Ok(())
+}
